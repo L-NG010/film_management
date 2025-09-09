11bb5fe000eec0b803ea101f6011461de0dd1fd5
L-NG010 <langnatanegara73@gmail.com>
2025-09-09 08:06:00 +0700

    menambah gambar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -43,6 +43,7 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Daftar Film'),
+        centerTitle: true,
         backgroundColor: Colors.blue,
         foregroundColor: Colors.white,
       ),
